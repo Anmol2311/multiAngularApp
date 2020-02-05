1c3049604506c31ba55f7f266f30ca63ced9256d
unknown <anu.meet23@gmail.com>
2020-02-05 19:28:55 +0530

    Add MultiAngularApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminSharedModule } from 'projects/admin/src/app/app.module';
+import { ClientSharedModule } from 'projects/client/src/app/app.module';
 
-const routes: Routes = [];
+// import {} from '../../projects/admin/src/app/app.module'
+const routes: Routes = [
+  {
+    path:'admin',
+    loadChildren:'../../projects/admin/src/app/app.module#AdminSharedModule'
+  },
+  {
+    path:'client',
+    loadChildren:'../../projects/client/src/app/app.module#ClientSharedModule'
+  },
+  {
+    path:'**',
+    redirectTo:'admin/one'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AdminSharedModule.forRoot(),
+    ClientSharedModule.forRoot()
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminSharedModule } from 'projects/admin/src/app/app.module';
+import { ClientSharedModule } from 'projects/client/src/app/app.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminSharedModule.forRoot(),
+    ClientSharedModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
